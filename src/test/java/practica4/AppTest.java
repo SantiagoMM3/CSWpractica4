f43b02c244622dbf4cdd29db2f363fd46471072a
S <smatos@alu.ucam.edu>
2022-07-10 12:17:10 +0100

    9

--- src/test/java/practica4/AppTest.java
+++ src/test/java/practica4/AppTest.java
@@ -1,5 +1,6 @@
 package practica4;
 
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class AppTest {
@@ -10,34 +11,42 @@ public class AppTest {
 	Estudiante estudiante2 = new Estudiante("Patricia", "Garcia", 2002, 1, asignaturas2);
 	Estudiante estudiante3 = null;
 	
+	@Test
 	public void testAssertion1() {
 		assertEquals(estudiante1.calcularEdad(), 22);
 	}
 	
+	@Test
 	public void testAssertion2() {
 		assertTrue(estudiante1.getNumeroAsignaturasPendientes() == 3);
 	}
 	
+	@Test
 	public void testAssertion3() {
 		assertFalse(estudiante2.getNumeroAsignaturasPendientes() == 3);
 	}
 	
+	@Test
 	public void testAssertion4() {
 		assertNotNull(estudiante1);
 	}
 	
+	@Test
 	public void testAssertion5() {
 		assertNull(estudiante3);
 	}
 	
+	@Test 
 	public void testAssertion6() {
 		assertSame(estudiante1, estudiante1);
 	}
 	
+	@Test
 	public void testAssertion7() {
 		assertNotSame(estudiante1, estudiante2);
 	}
 	
+	@Test
 	public void testAssertion8() {
 		assertArrayEquals(estudiante1.getAsignaturasPendientes(), asignaturas1);
 	}
